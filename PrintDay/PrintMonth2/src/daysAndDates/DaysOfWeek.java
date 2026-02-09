066f2219ff71e007ec348b4f710d9ad63e662f63
Isaac <garciai0526@hotmail.com>
2026-02-09 14:46:21 -0500

    fixed bugs to get program running successfully

--- PrintDay/PrintMonth2/src/daysAndDates/DaysOfWeek.java
+++ PrintDay/PrintMonth2/src/daysAndDates/DaysOfWeek.java
@@ -1,8 +1,8 @@
-package daysAndDates
+package daysAndDates;
 
 public class DaysOfWeek {
 	public static String DayOfWeekStr(int NumberOfDay) {
-		String dayStr = ""
+		String dayStr = "";
 		switch (NumberOfDay) {
 			case 1:
 				dayStr = "Sunday";
@@ -11,20 +11,21 @@ public class DaysOfWeek {
 				dayStr = "Monday";
 				break;
 			case 3:
-				dayStr = "Tuesday"
+				dayStr = "Tuesday";
 				break;
 			case 4:
 				dayStr = "Wednesday";
+				break;
 			case 5:
 				dayStr = "Thursday";
 				break;
 			case 6:
-				dayStr = "Thursday";
+				dayStr = "Friday";
 				break;
 			case 7:
 				dayStr = "Saturday";
 				break;
 		}
-		
+		return dayStr;
 	}
 }
